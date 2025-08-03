26ac276577d8ea5a8f670795c4018866458903a3
chaima-99 <chaimaamali47@gmail.com>
2025-08-03 22:37:03 +0100

    update the packages file

--- OWL/vite.config.js
+++ OWL/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import tailwindScrollbarHide from 'tailwind-scrollbar-hide'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [tailwindcss(), react(), tailwindScrollbarHide()],
+  plugins: [tailwindcss(), react()],
 })
