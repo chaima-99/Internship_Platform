b08d8ca1d7aca13e6f7068efbc1fd49e3052dca2
chaima-99 <chaimaamali47@gmail.com>
2025-08-03 23:17:55 +0100

    Fix Navbar.jsx file casing to match import

--- OWL/src/App.jsx
+++ OWL/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import Features from './components/Features';
 import Internships from './components/Internships';
 import About from './components/About';
